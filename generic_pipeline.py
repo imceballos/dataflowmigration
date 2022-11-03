3b1dda69f35261de7f03505e88bc1075c7993188
Israel Ceballos <imceballos@uc.cl>
2022-11-03 13:53:48 -0300

    First commit

---
+++ generic_pipeline.py
@@ -0,0 +1,16 @@
+import apache_beam as beam
+
+class SplitWords(beam.DoFn):
+  def __init__(self, delimiter=' '):
+    self.delimiter = delimiter
+
+  def process(self, text):
+      elements = {f"c{n}":word for n,word  in enumerate(text.split(self.delimiter))}
+      yield elements
+
+
+with beam.Pipeline() as pipeline:
+    header = (pipeline
+        | 'Read' >> beam.io.ReadFromText('inputexample.txt', skip_header_lines=5)
+        | 'ConvertToDict' >> beam.ParDo(SplitWords())
+        | 'Print' >> beam.Map(print))
